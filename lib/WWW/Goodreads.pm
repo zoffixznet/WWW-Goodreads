edf782b8a5ed123c680fdf432afde41ee8169ae2
Zoffix Znet <cpan@zoffix.com>
2014-01-26 17:11:09 -0500

    Converted to dzil

--- lib/WWW/Goodreads.pm
+++ lib/WWW/Goodreads.pm
@@ -1,141 +1,50 @@
 package WWW::Goodreads;
 
-use 5.006;
 use strict;
-use warnings FATAL => 'all';
+use warnings;
+
+# VERSION
+
+
+q|
+The fantastic element that explains the appeal of games to many developers
+is neither the fire-breathing monsters nor the milky-skinned, semi-clad
+sirens; it is the experience of carrying out a task from start to finish
+without any change in the user requirements.
+|;
+
+__END__
+
+=encoding utf8
 
 =head1 NAME
 
-WWW::Goodreads - The great new WWW::Goodreads!
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
-
-our $VERSION = '0.01';
-
+WWW::Goodreads - www.goodreads.com API implementation
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
+=head1 REPOSITORY
 
-Perhaps a little code snippet.
-
-    use WWW::Goodreads;
-
-    my $foo = WWW::Goodreads->new();
-    ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 SUBROUTINES/METHODS
-
-=head2 function1
-
-=cut
-
-sub function1 {
-}
-
-=head2 function2
-
-=cut
-
-sub function2 {
-}
-
-=head1 AUTHOR
-
-Zoffix Znet, C<< <zoffix at cpan.org> >>
+Fork this module on GitHub:
+L<https://github.com/zoffixznet/WWW-Lipsum>
 
 =head1 BUGS
 
-Please report any bugs or feature requests to C<bug-www-goodreads at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=WWW-Goodreads>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
+To report bugs or request features, please use
+L<https://github.com/zoffixznet/WWW-Lipsum/issues>
 
+If you can't access GitHub, you can email your request
+to C<bug-www-lipsum at rt.cpan.org>
 
+=head1 AUTHOR
 
+Zoffix Znet <zoffix at cpan.org>
+(L<http://zoffix.com/>, L<http://haslayout.net/>)
 
-=head1 SUPPORT
+=head1 LICENSE
 
-You can find documentation for this module with the perldoc command.
-
-    perldoc WWW::Goodreads
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=WWW-Goodreads>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/WWW-Goodreads>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/WWW-Goodreads>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/WWW-Goodreads/>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
-=head1 LICENSE AND COPYRIGHT
-
-Copyright 2013 Zoffix Znet.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of the the Artistic License (2.0). You may obtain a
-copy of the full license at:
-
-L<http://www.perlfoundation.org/artistic_license_2_0>
-
-Any use, modification, and distribution of the Standard or Modified
-Versions is governed by this Artistic License. By using, modifying or
-distributing the Package, you accept this license. Do not use, modify,
-or distribute the Package, if you do not accept this license.
-
-If your Modified Version has been derived from a Modified Version made
-by someone other than you, you are nevertheless required to ensure that
-your Modified Version complies with the requirements of this license.
-
-This license does not grant you the right to use any trademark, service
-mark, tradename, or logo of the Copyright Holder.
-
-This license includes the non-exclusive, worldwide, free-of-charge
-patent license to make, have made, use, offer to sell, sell, import and
-otherwise transfer the Package with respect to any patent claims
-licensable by the Copyright Holder that are necessarily infringed by the
-Package. If you institute patent litigation (including a cross-claim or
-counterclaim) against any party alleging that the Package constitutes
-direct or contributory patent infringement, then this Artistic License
-to you shall terminate on the date that such litigation is filed.
-
-Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
-AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
-THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
-YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
-CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
-CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
-EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
+You can use and distribute this module under the same terms as Perl itself.
+See the C<LICENSE> file included in this distribution for complete
+details.
 
 =cut
-
-1; # End of WWW::Goodreads
